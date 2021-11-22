5578bcb7fe2758e7585b23b6fbf65056ffc2bb36
alevans99 <alevans99@gmail.com>
2021-11-22 11:56:43 +0000

    allows cross origin resource sharing

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const apiRouter = require("./routes/api-router");
+const express = require('express');
+const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 const {
-    handle500Errors,
-    handleCustomErrors,
-    handlePsqlErrors,
-} = require("./controllers/error-controllers");
-const {
-    connected
-} = require("./utils");
-const connectedRouter = require("./routes/connected-router");
+  handle500Errors,
+  handleCustomErrors,
+  handlePsqlErrors,
+} = require('./controllers/error-controllers');
+const { connected } = require('./utils');
+const connectedRouter = require('./routes/connected-router');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.use("/", connectedRouter)
+app.use('/', connectedRouter);
 
 //Anything not routed through /api returns an error
-app.all("/*", (req, res) => {
-    res.status(404).send({
-        message: "path not found"
-    });
+app.all('/*', (req, res) => {
+  res.status(404).send({
+    message: 'path not found',
+  });
 });
 
 app.use(handleCustomErrors);
